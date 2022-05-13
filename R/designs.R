72da1680849470292846d4f8213c9aa7cf3c062d
labja <99743799+labja@users.noreply.github.com>
2022-05-13 15:59:53 +0200

    Update designs.R

--- R/designs.R
+++ R/designs.R
@@ -76,7 +76,7 @@ determine_dose_r6 <- function(res_dlt,res_time,t,tox_rates) {
     
     # 6 patients have been assigned to last dose and some results are pending -> wait
     n_dlt_last_dose <- all_summary %>% filter(dose==last_dose) %>% select(n_dlt) %>% as.numeric() %>% tidyr::replace_na(0)
-    t <- ifelse(n_dlt_last_dose==1,max(res_time$t_end_treat),nth(sort(res_time$t_end_treat,decreasing=TRUE),2)
+    t <- ifelse(n_dlt_last_dose==1,max(res_time$t_end_treat),nth(sort(res_time$t_end_treat,decreasing=TRUE),2))
     #t <- max(res_time$t_end_treat) 
     dose_decision <- determine_dose_r6(res_dlt=res_dlt,res_time=res_time,t=t,tox_rates=tox_rates)
     new_dose <- dose_decision$dose
